6f008d8a1a16dda9f8aef1d10add385f8a53e5a4
Kornelia Szabo <kszabo@thoughtworks.com>
2018-10-26 15:12:58 +0200

    Working with draganddrop contextx to visualize data better

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React, { Component } from 'react';
-import './App.css';
+import '@atlaskit/css-reset';
 import initialData from "./initial-data";
 import Column from "./Column";
+import { DragDropContext} from 'react-beautiful-dnd';
 
 class App extends Component {
   state = initialData;
 
   render() {
     return (
-      <div className="App">
-          <p>
+      <DragDropContext>
             {this.state.columnOrder.map((columnId) => {
               const column = this.state.columns[columnId];
               const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
 
               return <Column key={column.id} column={column} tasks={tasks}/>;
-            })
-            }
-          </p>
-      </div>
+            })}
+      </DragDropContext>
     );
   }
 }
--- src/Column.jsx
+++ src/Column.jsx
@@ -1,11 +1,26 @@
 import React, {Component} from 'react';
+import styled from 'styled-components';
+import Task from "./Task";
+
+const Container = styled.div`
+margin: 8px;
+border: 1px solid lightgrey;
+border-radius: 2px;
+`;
+const Title = styled.h3`
+padding: 8px;
+`;
+const TaskList = styled.div`
+padding: 8px;
+`;
 
 class Column extends Component {
   render() {
     return (
-      <div>
-        <h1>{this.props.column.title}</h1>
-      </div>
+      <Container>
+        <Title>{this.props.column.title}</Title>
+        <TaskList>{this.props.tasks.map((task) => <Task key={task.id} task={task}/>)}</TaskList>
+      </Container>
     )
   }
 }
